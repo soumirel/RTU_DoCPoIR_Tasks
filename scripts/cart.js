34803ca8bb384d3c2527f42ba8cf39c3d61d183c
Mazanov Aleksei <soumirel.rofl@gmail.com>
2022-11-24 00:12:57 +0300

    Task 11 completed FINALY AFTER 12 HOURS

--- scripts/cart.js
+++ scripts/cart.js
@@ -80,12 +80,22 @@ class GoodCard {
     }
 }
 
+class GoodInCart {
+    good = null;
+    cartNode = null;
+    constructor(good, cartNode) {
+        this.good = good;
+        this.cartNode = cartNode;
+    }
+}
+
 var cart = [];
 var goodCards = [];
 var boxes = document.querySelectorAll(".box");
-initialiseGoods();
+var goodCardPrototype = document.querySelector(".cart-good");
+initializeGoods();
 
-function initialiseGoods() {
+function initializeGoods() {
     for (i = 0; i < boxes.length; i++) 
     {
         let currentName = boxes[i].querySelector(".box-name").textContent;
@@ -104,18 +114,78 @@ function initialiseGoods() {
         });
         let currentGoodCard = new GoodCard(currentGood, currentAddToCartButton);
         goodCards.push(currentGoodCard);
+        
     }
 }
 
 function addToCart(good) {
-    let newCartCard = document.createElement('div');
-    if (good.company == "AMD") {
-        newCartCard.className = "cart-good amd-good";
+    let newCartNode = goodCardPrototype.cloneNode(true);
+    let newGoodInCart = new GoodInCart(Object.assign({}, good), newCartNode);
+    
+    if (newGoodInCart.good.company == "AMD") {
+        newGoodInCart.cartNode.className = "cart-good amd-good";
     } else {
-        newCartCard.className = "cart-good intel-good";
+        newGoodInCart.cartNode.className = "cart-good intel-good";
     }
-    newCartCard.innerHTML = good.name + good.price + "$";
-    cart_content.append(newCartCard);
-    cart.push(good);
+    newGoodInCart.cartNode.querySelector(".good-name").textContent = newGoodInCart.good.name;
+
+    newGoodInCart.cartNode.querySelector(".good-price").textContent = newGoodInCart.good.price;
+
+    newGoodInCart.cartNode.querySelector(".goods-amount").textContent = "1";
+
+    newGoodInCart.cartNode.querySelector(".cart-minus-btn").classList.add("disabled");
+
+    showToltalPrice(newGoodInCart);
+    
+    newGoodInCart.cartNode.querySelector(".cart-plus-btn").addEventListener('click', () => {
+        plus(newGoodInCart);
+    });
+
+    newGoodInCart.cartNode.querySelector(".cart-minus-btn").addEventListener('click', () => {
+        minus(newGoodInCart);
+    });
+
+    newGoodInCart.cartNode.querySelector(".cart-bin-btn").addEventListener('click', () => {
+        bin(newGoodInCart);
+    });
+
+    cart_content.append(newGoodInCart.cartNode);
+    newGoodInCart.cartNode.classList.remove("hidden-cart-good");
+    cart.push(newGoodInCart);
+}
+
+function showToltalPrice(goodInCart) {
+    let amount =  parseInt(goodInCart.cartNode.querySelector(".goods-amount").textContent);
+    let totalPrice = goodInCart.good.price * amount;
+    goodInCart.cartNode.querySelector(".total-good-price").textContent = totalPrice + '$';
+}
+
+
+function plus(goodInCart) {
+    let amount =  parseInt(goodInCart.cartNode.querySelector(".goods-amount").textContent);
+    amount++;
+    goodInCart.cartNode.querySelector(".goods-amount").textContent = amount;
+    showToltalPrice(goodInCart);
+    if (amount > 1) {
+        goodInCart.cartNode.querySelector(".cart-minus-btn").classList.remove("disabled");
+    }
+}
+
+function minus(goodInCart) {
+    let amount =  parseInt(goodInCart.cartNode.querySelector(".goods-amount").textContent);
+    if (amount > 1) {
+        amount--;
+        goodInCart.cartNode.querySelector(".goods-amount").textContent = amount;
+        showToltalPrice(goodInCart);
+    }
+    if (amount == 1) {
+        goodInCart.cartNode.querySelector(".cart-minus-btn").classList.add("disabled");
+    }
+}
+
+function bin(goodInCart) {
+    let index = cart.indexOf(goodInCart);
+    cart.splice(index, 1);
+    goodInCart.cartNode.remove();
 }
 
